7eeb55ec52c4aaa75d0402c30e947ed55e3badcb
BadPi88 <miguel88martin@gmail.com>
2023-02-21 03:18:08 +0100

    Modelo
    Terminado hasta método modificar

--- src/main/java/org/iesalandalus/programacion/alquilervehiculos/modelo/Modelo.java
+++ src/main/java/org/iesalandalus/programacion/alquilervehiculos/modelo/Modelo.java
@@ -1,5 +1,61 @@
 package org.iesalandalus.programacion.alquilervehiculos.modelo;
 
-public class Modelo {
+import javax.naming.OperationNotSupportedException;
 
+import org.iesalandalus.programacion.alquilervehiculos.modelo.dominio.Alquiler;
+import org.iesalandalus.programacion.alquilervehiculos.modelo.dominio.Cliente;
+import org.iesalandalus.programacion.alquilervehiculos.modelo.dominio.Turismo;
+import org.iesalandalus.programacion.alquilervehiculos.modelo.negocio.Alquileres;
+import org.iesalandalus.programacion.alquilervehiculos.modelo.negocio.Clientes;
+import org.iesalandalus.programacion.alquilervehiculos.modelo.negocio.Turismos;
+
+public class Modelo {
+	private Clientes clientes;
+	private Alquileres alquileres;
+	private Turismos turismos;
+
+	public void comenzar() {
+		clientes = new Clientes();
+		alquileres = new Alquileres();
+		turismos = new Turismos();
+	}
+
+	public void terminar() {
+		System.out.print("El modelo ha terminado");
+	}
+
+	// insertar cliente y turismo
+	public void insertar(Cliente cliente) throws OperationNotSupportedException {
+		clientes.insertar(new Cliente(cliente));
+	}
+
+	public void insertar(Turismo turismo) throws OperationNotSupportedException {
+		turismos.insertar(new Turismo(turismo));
+
+	}
+
+	public void insertar(Alquiler alquiler) throws OperationNotSupportedException {
+
+		alquileres.insertar(new Alquiler(alquiler));
+	}
+
+	//buscar cliente alquiler turismo            
+	public Cliente buscar(Cliente cliente) {
+		return new Cliente(clientes.buscar(cliente));
+	}
+
+	public Alquiler buscar(Alquiler alquiler) {
+		return new Alquiler(alquileres.buscar(alquiler));
+	}
+
+	public Turismo buscar(Turismo turismo) {
+		return new Turismo(turismos.buscar(turismo));
+	}
+
+	public void modificar(Cliente cliente, String nombre, String telefono) throws OperationNotSupportedException {
+		clientes.modificar(cliente, nombre, telefono);
+	}
+	
+	
+	
 }
